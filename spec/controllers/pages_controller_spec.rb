addd2e9beeea02d43c76d63a740ad485530473f1
lasse christiansen <webdevlasse@gmail.com>
2012-01-26 11:56:22 -0500

    completed the layout structure

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
 
   describe "GET 'home'" do
@@ -12,14 +16,15 @@ describe PagesController do
   
   it "should have the right title" do
     get 'home'
-    response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+    response.should have_selector("title", 
+                                :content => "#{@base_title} | Home")
     end
   end
   
   it "should have a non-blank body" do
     get 'home'
     response.body.should_not =~ /<body>\s*<\/body>/
- end
+  end
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
@@ -27,7 +32,8 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", 
+                                      :content => "#{@base_title}  | Contact")
     end
   end
  
@@ -38,7 +44,8 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", 
+                                      :content => "#{@base_title}  | About")
     end
   end
 end
