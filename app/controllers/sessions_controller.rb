b1796aee33d81e862b92cbe330922608411a0a35
lasse christiansen <webdevlasse@gmail.com>
2012-02-27 18:24:47 -0500

    Handling invalid sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,16 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      flash.now[:error] = "Invalid email/password combination"
+      render 'new'
+    else
+      #handle successful signin here
   end
   
   def destroy
   end
-
+end 
 end
