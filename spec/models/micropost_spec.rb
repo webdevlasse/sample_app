d0d7761d90987f122261cacc9e728625a9c2f94f
lasse christiansen <webdevlasse@gmail.com>
2012-03-26 16:31:12 -0400

    user relationships following/followers

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -54,3 +54,15 @@ end
 #  updated_at :datetime
 #
 end
+
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -161,13 +161,13 @@ end
   end
   
   describe "micropost associations" do
-    
-    before(:each) do
-         @user = User.create(@attr)
-         @mp1 = Factory(:micropost, :user => @user, :created_at => 1.day.ago)
-         @mp2 = Factory(:micropost, :user => @user, :created_at => 1.hour.ago)
-       end
-       
+  #     
+  #     before(:each) do
+  #          @user = User.create(@attr)
+  #          @mp1 = Factory(:micropost, :user => @user, :created_at => 1.day.ago)
+  #          @mp2 = Factory(:micropost, :user => @user, :created_at => 1.hour.ago)
+  #        end
+  #              
  
     # it "should have a microposts attribute" do
     #       @user.should respond_to(:microposts)
@@ -203,7 +203,55 @@ end
     #         
     #       end
     #      end          
-    end  
+   # describe "relationships " do
+   #        before (:each) do
+   #           @user = User.create!(@attr) 
+   #           @followed = Fcatory(:user)
+   #        end                           
+   #        
+   #        it "should have a relationships method" do
+   #          @user.should respond_to(:relationships)
+   #          
+   #        end 
+   #        
+   #        it "should have a following method" do
+   #          @user.should respond_to(:following)
+   #        end 
+   #        
+   #        it "should follow another user" do
+   #          @user.follow!(@followed)
+   #          @user.should be_followin
+   #        end  
+   #        
+   #        it "should include the followed user in the following array" do
+   #          @user.follow!(@followed)
+   #          @user.following.should include(@followed)
+   #        end
+   #        
+   #        it "should have an unfollow! method" do
+   #             @followed.should respond_to(:unfollow!)
+   #        end
+   # 
+   #        it "should unfollow a user" do
+   #          @user.follow!(@followed)
+   #          @user.unfollow!(@followed)
+   #          @user.should_not be_following(@followed)
+   #        end
+   #        
+   #        it "should have a reverse_relationships method" do
+   #            @user.should respond_to(:reverse_relationships)
+   #        end
+   #           
+   #        it "should have a followers method" do
+   #          @user.should respond_to(:followers)
+   #        end
+   # 
+   #        it "should include the follower in the followers array" do
+   #          @user.follow!(@followed)
+   #          @followed.followers.should include(@user)
+   #        end          
+   #    end          
+  end 
 
 
 
